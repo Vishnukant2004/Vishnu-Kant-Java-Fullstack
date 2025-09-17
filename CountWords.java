a178e30c77fc28ca729959e2b5fb3e77d54dbe7e
VISHNU KANT2004 <sharmavishnukant100@gmail.com>
2025-09-18 00:14:10 +0530

    Add files via upload

---
+++ CountWords.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class CountWords {
+  public static void main(String[] args) { 
+      Scanner sc = new Scanner("vishnu kant sharma");
+      int count = 0;
+      while (sc.hasNext()){
+        count++;
+        sc.next();
+        // counting
+        
+      }
+      System.out.println("The count is "+count);   
+  }  
+}
